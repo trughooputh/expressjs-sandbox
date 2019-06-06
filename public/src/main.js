020a636c8b68fff4dfdb8863a88d0678ebc46f78
Thomas Rughooputh <thomas.rughooputh@gmail.com>
2019-06-06 21:39:07 +0200

    Add vue components + webpack config

---
+++ public/src/main.js
@@ -0,0 +1,10 @@
+'use strict'
+import Vue from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './components/App.vue'
+
+new Vue({
+  el: 'app',
+  components: {App},
+  methods: {}
+})
